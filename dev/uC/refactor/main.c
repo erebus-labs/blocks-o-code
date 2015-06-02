a047d7f87c937fecd0cf5c7e24c98f9f7e13a26e
Greg Stromire <gsmolodrone@gmail.com>
2015-06-02 13:22:52 -0700

    Chain with 8 blocks, but one at a time?

--- dev/uC/refactor/main.c
+++ dev/uC/refactor/main.c
@@ -13,6 +13,7 @@
 
 #define INPUT_BLOCK	0
 #define POTENTIOMETER 0
+#define TESTING_BLOCK 0
 
 // hard-coded test values for prototyping
 
@@ -26,7 +27,7 @@ typedef enum {
 	Print
 } BlockFunc;
 
-static const BlockFunc function = A_Var;
+static const BlockFunc function = 0;
 #endif
 
 
@@ -53,19 +54,27 @@ typedef enum {
 	ErrorLedOnCommand,
 	ErrorLedBlinkCommand,
 	ErrorLedOffCommand,
+	Reset,
 	AuxiliaryRead,
 	
 	ReadCommandCount	// Unused, always last
 } ReadCommands;
 
 static volatile uint16_t workQueue	= 0;
+static volatile uint8_t	tOVCount	= 0;
+static volatile uint8_t	tCountFlag	= 0;
+static volatile uint8_t statusLED	= 0;
+static volatile uint8_t errorLED	= 0;
+
 
 static void initIO(void);
 static void startupSequence(void);
 static uint8_t GlobalBusCommand(uint8_t command);
 static uint8_t ABCReadData(void);
 static void serviceWorkqueue(void);
-static void initTimer(void);
+static inline void initTimer(void);
+static inline void enableTimer(void);
+static inline void disableTimer(void);
 
 #if INPUT_BLOCK
 static uint8_t ABCAuxiliary(void);
@@ -77,9 +86,12 @@ int main(void) {
 	startupSequence();
 	startupSequence();
 	
+#if TESTING_BLOCK
+	uint8_t addr = 42;
+#else
 	// spin until position received from connected block (or main processor)
 	uint8_t addr = waitForVector();
-//	uint8_t addr = 42;
+#endif
 	
 	// assign function pointer to use custom data-collector function
 	GetData_ptr GlobalFunc_ptr = GlobalBusCommand;
@@ -93,10 +105,14 @@ int main(void) {
 	// continuously poll i2c for commands
 	while (loop_i2c()) {
 		serviceWorkqueue();
+		
+#if TESTING_BLOCK
+#else
 		updateChain();
+#endif
 		
 #if POTENTIOMETER
-		ADCSRA |= (1<<ADSC);
+		ADCSRA |= _BV(ADSC);
 #endif
 		
 		// timer led blink?
@@ -105,22 +121,63 @@ int main(void) {
 }
 
 void initIO(void) {
+	
+	// disable WDT as necessary after reset
+	MCUSR = 0;
+	WDTCR |= _BV(WDE) | _BV(WDCE);
+	WDTCR  = 0;
 
 #if POTENTIOMETER
 	// start adc
 	setup_adc();
 #endif
 	
+	// setup timer
+	initTimer();
+	
 	// set output pins
 	DDRA |= _BV(STATUS_LED);
 	DDRA |= _BV(ERROR_LED);
 }
 
+/**
+ *	Timer counts to 0.5 second by using a prescaler and overflow,
+ *	(8,000,000Hz/128 prescaler/250 overflows/125 times).
+ */
 void initTimer(void) {
 	
-	// set timer1 presaler to 128
-	TCCR1B |= _BV(CS13);
-//	OCR1B
+	TCCR1B  |= _BV(CS13);				// presaler to 64
+	TIFR	|= _BV(TOV1);				// set overflow interrupt
+	TCNT1	 = 0;						// initialize counter to zero
+}
+
+void enableTimer(void) {
+	
+	TIMSK	|= _BV(TOIE1);				// set interrupt enable flag
+}
+
+void disableTimer(void) {
+	if (statusLED | errorLED) {
+		return;
+	}
+	TIMSK	&= ~_BV(TOIE1);				// clear interrupt enable flag
+}
+
+ISR(TIMER1_OVF_vect) {
+	++tOVCount;
+	if (tOVCount >= 64) {
+		if (statusLED) {
+			TOGGLE_STATUS();
+		}
+		tOVCount = 0;
+		++tCountFlag;
+		if (tCountFlag >= 2) {
+			if (errorLED) {
+				TOGGLE_ERROR();
+				tCountFlag = 0;
+			}
+		}
+	}
 }
 
 void startupSequence(void) {
@@ -152,7 +209,7 @@ static uint8_t GlobalBusCommand(uint8_t command) {
 
 #if INPUT_BLOCK
 static uint8_t ABCAuxiliary(void) {
-	return 0b00000000 & read_adc();
+	return 0b00001111 & read_adc();
 }
 #endif
 
@@ -188,48 +245,55 @@ static void serviceWorkqueue(void) {
 	
 	// Status On
 	if (workQueue & _BV(StatusLedOnCommand)) {
+		statusLED = 0;
+		disableTimer();
 		STATUS_PORT |= _BV(STATUS_LED);
 		workQueue &= ~_BV(StatusLedOnCommand);
 	}
 	
 	// Status Blink
 	if (workQueue & _BV(StatusLedBlinkCommand)) {
-		// may move to timer
-		STATUS_PORT &= ~_BV(STATUS_LED);
-		TOGGLE_STATUS();
-		_delay_ms(50);
-		TOGGLE_STATUS();
-		_delay_ms(50);
+		statusLED = 1;
+		enableTimer();
 		workQueue &= ~_BV(StatusLedBlinkCommand);
 	}
 	
 	// Status Off
 	if (workQueue & _BV(StatusLedOffCommand)) {
+		statusLED = 0;
+		disableTimer();
 		STATUS_PORT &= ~_BV(STATUS_LED);
 		workQueue &= ~_BV(StatusLedOffCommand);
 	}
 	
 	// Error On
 	if (workQueue & _BV(ErrorLedOnCommand)) {
+		errorLED = 0;
+		disableTimer();
 		ERROR_PORT |= _BV(ERROR_LED);
 		workQueue &= ~_BV(ErrorLedOnCommand);
 	}
 	
 	// Error Blink
 	if (workQueue & _BV(ErrorLedBlinkCommand)) {
-		// may move to timer
-		ERROR_PORT &= ~_BV(ERROR_LED);
-		TOGGLE_ERROR();
-		_delay_ms(50);
-		TOGGLE_ERROR();
-		_delay_ms(50);
+		errorLED = 1;
+		enableTimer();
 		workQueue &= ~_BV(ErrorLedBlinkCommand);
 	}
 	
 	// Error Off
 	if (workQueue & _BV(ErrorLedOffCommand)) {
+		errorLED = 0;
+		disableTimer();
 		ERROR_PORT &= ~_BV(ERROR_LED);
 		workQueue &= ~_BV(ErrorLedOffCommand);
+	}
+	
+	// Reset MCU
+	if (workQueue & _BV(Reset)) {
+		// do any other work as necessary...
+		workQueue &= ~_BV(Reset);
+		WDTCR |= _BV(WDE);
 	}
 	
 	// Auxiliary Read
